0ab41032120ff8adecf449d0c2178e980a99e8ff
Revym <michal.cychner@gmail.com>
2025-07-05 20:02:45 +0200

    Disappearing crosshair

--- Assets/Scripts/ads.cs
+++ Assets/Scripts/ads.cs
@@ -6,16 +6,25 @@ public class SimpleADS : MonoBehaviour
     [Header("Positions (local)")]
     public Vector3 hipPosition = new Vector3(0.3f, -0.19f, 0.94f);
     public Vector3 adsPosition = new Vector3(0f, -0.19f, 0.94f);
+    public string crosshairName = "SimpleCrosshair";
 
     [Header("Settings")]
     public float   transitionSpeed = 10f;  // im wyższa, tym szybsze przejście
 
     private Vector3 targetPosition;
 
+    private GameObject crosshair;
+
     void Start()
     {
+        // podłączenie do crosshair
+        crosshair = GameObject.Find(crosshairName);
+        if(crosshair==null)
+            Debug.LogError("Couldnt find object "+crosshairName);
+
         // ustaw początkowo hip‐fire
         transform.localPosition = hipPosition;
+        crosshair?.SetActive(true);
     }
 
     void Update()
@@ -30,5 +39,9 @@ public class SimpleADS : MonoBehaviour
             targetPosition,
             Time.deltaTime * transitionSpeed
         );
+
+        if (crosshair!=null)
+            crosshair.SetActive(!aiming);
+
     }
 }
